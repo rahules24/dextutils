c79c2643d883b15f43d2b2f8cf2fd7e1966f8289
Rahul <rahul.es2024@gmail.com>
2024-11-06 00:40:11 +0100

    x11

--- dextutils/settings.py
+++ dextutils/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-&l-vqn$i=hqyw2is4ca2#m63tg+k+vrupg+8zo6$36a9cw_7no"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['dextutils.onrender.com', '127.0.0.1', 'localhost']
 
